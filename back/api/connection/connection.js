04a017dffb1808a5cc7cc7a7d56d0dc66ba20657
Zerotet <brian.sebasmart@gmail.com>
2023-05-14 01:18:07 -0300

    base

---
+++ back/api/connection/connection.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+const mysqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'tealingo',
+  port: '8889'
+});
+
+mysqlConnection.connect( err => {
+  if(err){
+    console.log('Error en db: ', err);
+    return;
+  }else{
+    console.log('Conectado correctamente a la bd');
+  }
+});
+
+module.exports = mysqlConnection;
